db3a54138a572b7f529d4d19f64b64740cf7964d
ρяє∂αтσя <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-11-01 15:46:38 +0530

    Update forcesub.py

--- TeamTeleRoid/forcesub.py
+++ TeamTeleRoid/forcesub.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 async def ForceSub(bot: Client, cmd: Message):
     try:
-        user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=Message.from_user.id)
+        user = await get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=Message.from_user.id)
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
